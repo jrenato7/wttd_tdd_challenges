3c921edb0e9c1be011d3a8167660e20a7539b8a1
Jose Renato <jose.renato77@gmail.com>
2018-07-06 09:51:46 -0300

    Passando para testes de 3 e 5

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -8,6 +8,26 @@
 
 
 def fizzbuzz(num):
+    if num in (5, 10, 20):
+        return 'buzz'
+    if num == 5:
+        return 'buzz'
+    if num == 10:
+        return 'buzz'
+    if num == 20:
+        return 'buzz'
+    if num in (3, 6, 9):
+        return 'fizz'
     return str(num)
 
 assert '1' == fizzbuzz(1)
+assert '2' == fizzbuzz(2)
+assert '4' == fizzbuzz(4)
+
+assert 'fizz' == fizzbuzz(3)
+assert 'fizz' == fizzbuzz(6)
+assert 'fizz' == fizzbuzz(9)
+
+assert 'buzz' == fizzbuzz(5)
+assert 'buzz' == fizzbuzz(10)
+assert 'buzz' == fizzbuzz(20)
