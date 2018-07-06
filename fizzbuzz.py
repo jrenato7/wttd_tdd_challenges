2c6b6b887ff21392d71f560c3d8cc169992f38fb
Jose Renato <jose.renato77@gmail.com>
2018-07-05 23:45:06 -0300

    Primeiro teste do programa fizzbuzz

---
+++ fizzbuzz.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+"""
+1. Se receber um número divisível por 3 fala fizz
+2. Se receber um número divisível por 5 fala buzz
+3. Se receber um número divisível por 3 e 5 fala fizzbuzz
+4. Se não for nenhum dos acima fala o próprio número
+"""
+
+
+def fizzbuzz(num):
+    return str(num)
+
+assert '1' == fizzbuzz(1)
