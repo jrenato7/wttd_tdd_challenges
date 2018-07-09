92b273af2727224aac543150475533c1dede95c5
Jose Renato <jose.renato77@gmail.com>
2018-07-09 16:18:17 -0300

    Add test csrf

--- eventex_baby_steps/eventex/subscriptions/tests.py
+++ eventex_baby_steps/eventex/subscriptions/tests.py
@@ -13,9 +13,15 @@ class SubscriptionTest(TestCase):
         self.assertTemplateUsed(response, 'subscriptions/subscription_form.html')
 
     def test_has_form(self):
+        """Must contain HTML form"""
         response = self.client.get('/inscricao/')
         self.assertContains(response, '<form', 1)
-        self.assertContains(response, '<input', 5)
+        self.assertContains(response, '<input', 6)
         self.assertContains(response, 'type="submit"', 1)
         self.assertContains(response, 'type="text"', 3)
-        self.assertContains(response, 'type="email"', 3)
+        self.assertContains(response, 'type="email"', 1)
+
+    def test_csrf(self):
+        """Must contain csrf token"""
+        response = self.client.get('/inscricao/')
+        self.assertContains(response, 'csrfmiddlewaretoken')
